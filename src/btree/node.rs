af4eba4945bcc424863c8354597366ea7c3a1afb
shunm-999 <shunmiyazaki7812@gmail.com>
2025-06-15 20:44:45 +0900

    検索の処理を追加

--- src/btree/node.rs
+++ src/btree/node.rs
@@ -6,19 +6,12 @@ pub(crate) struct BtreeNode {
     is_leaf: bool,
 }
 
-pub(crate) enum SearchResult {
-    Found(i32, i32),
-    Next(BtreeNode),
-    NotFound,
-}
-
 pub(crate) trait Search {
-    fn search(&self, key: i32) -> SearchResult;
+    fn search(&self, target_key: i32) -> Option<i32>;
 }
 
 impl Search for BtreeNode {
-    fn search(&self, key: i32) -> SearchResult {
-        let target_key = key;
+    fn search(&self, target_key: i32) -> Option<i32> {
         let mut found_index = None;
 
         self.keys.iter().enumerate().for_each(|(index, key)| {
@@ -29,26 +22,19 @@ impl Search for BtreeNode {
 
         if found_index != None {
             // そのノード内でkeyがヒットしたら、値を返す
-            return SearchResult::Found(target_key, self.values[found_index.unwrap()]);
+            return Some(self.values[found_index.unwrap()]);
         }
         if found_index == None && self.is_leaf {
             // keyがヒットしないかつ、根ノードの場合はkeyは存在しない
-            return SearchResult::NotFound;
+            return None;
         }
 
-        let mut child_index = None;
-        self.keys.windows(2).enumerate().for_each(|(index, pair)| {
-            let (previous, next) = (&pair[0], &pair[1]);
-            if previous < &target_key && &target_key < next {
-                child_index = Some(index);
-            }
-        });
+        let child_index = self
+            .keys
+            .iter()
+            .position(|&k| target_key <= k)
+            .unwrap_or(self.keys.len());
 
-        if child_index != None {
-            let child = self.children[child_index.unwrap() as usize].clone();
-            SearchResult::Next(*child)
-        } else {
-            SearchResult::NotFound
-        }
+        self.children[child_index].search(target_key)
     }
 }
